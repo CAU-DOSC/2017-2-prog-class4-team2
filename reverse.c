e56e6486642ab5791f02bcaefdf8d8dc801b7542
qkralsrl1029 <33649883+qkralsrl1029@users.noreply.github.com>
2017-12-04 21:30:48 +0900

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,6 +1,6 @@
 char* T_rev(char*str, int n)
 {
-	char *temp = (char*)malloc(sizeof(char)*n);
+	char *temp = (char*)malloc(sizeof(char)*(n+1));
 	for (int i = 0; i < n; i++)
 		temp[i] = str[n - i - 1];
 	return temp;
