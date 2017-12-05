54e2d18d9a2b30ac177adf83c116daf5516a7f1b
qkralsrl1029 <33649883+qkralsrl1029@users.noreply.github.com>
2017-12-05 10:47:28 +0900

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,9 +1,18 @@
 char* T_rev(char*str, int n)
 {
+	clock_t=start,end;
+	double time;
+	double*ptime=&time;
+	
+	start=clock();
 	char *temp = (char*)malloc(sizeof(char)*(n+1));
 	for (int i = 0; i < n+10; i++)
 		temp[i] = NULL;
 	for (int i = 0; i < n; i++)
 		temp[i] = str[n - i - 1];
+	end=clock();
+	
+	time=end-start;
+	
 	return temp;
 }
